a8d18a68dee5674d9f31851843c53cae3771a056
brizzaa <brizzantex@gmail.com>
2025-10-21 14:51:06 +0200

    commit: routing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,12 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    port: 5173,
-    host: true,
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
   },
-  preview: {
-    port: 5173,
-    host: true,
-  },
+  base: "/",
 });
